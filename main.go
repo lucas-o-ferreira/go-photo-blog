6e240f7b56970c59004af0c8e2c45d44195780bd
Lucas Ferreira <134474361+lucas-o-ferreira@users.noreply.github.com>
2024-07-22 18:43:22 -0300

    challenge finished

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 var tpl *template.Template
@@ -13,10 +21,67 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.Handle("/public/", http.StripPrefix("/public", http.FileServer(http.Dir("./public"))))
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
-func index(w http.ResponseWriter, req *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+func index(w http.ResponseWriter, r *http.Request) {
+	c := cookie(w, r)
+	if r.Method == http.MethodPost {
+		mf, hf, err := r.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+
+		ext := strings.Split(hf.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+
+		// Create a new file
+
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+		// add filename to cookie
+		c = saveToCookie(w, c, fname)
+
+	}
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.gohtml", xs)
+}
+
+func cookie(w http.ResponseWriter, r *http.Request) *http.Cookie {
+	c, err := r.Cookie("session")
+	if err != nil {
+		sID := uuid.New()
+		c = &http.Cookie{
+			Name:  "session",
+			Value: sID.String(),
+		}
+		http.SetCookie(w, c)
+	}
+	return c
+}
+
+func saveToCookie(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
+	s := c.Value
+
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+	c.Value = s
+	http.SetCookie(w, c)
+	return c
 }
